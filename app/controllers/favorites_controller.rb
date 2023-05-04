342be8de7c36e3486b45c5b4afcfd763c30e2f85
EC2 Default User <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-04 02:52:01 +0000

    _favorite.html.erb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,16 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)#favoritesテーブルのbook_idカラムがbook.idのものを探す
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)#favoritesテーブルのbook_idカラムがbook.idのものを探す
     favorite.destroy
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 end
