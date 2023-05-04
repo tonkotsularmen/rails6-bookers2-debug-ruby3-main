2afc68bbc8e748154577fd2ce1ea43a99a7ad344
EC2 Default User <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-04 07:53:53 +0000

    応用課題06完成

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,19 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    #@book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    #redirect_to request.referer
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    #@book_comment = BookComment.new
+    #redirect_to request.referer
   end
 
   private
