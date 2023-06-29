e1b7497c344f77db5683f3f7060b33edda813583
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-29 13:34:07 +0000

    chat機能作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,26 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :books
-  has_many :favorites, dependent: :destroy
-  has_many :book_comments, dependent: :destroy
+  has_many :favorites,             dependent: :destroy
+  has_many :book_comments,         dependent: :destroy
+  has_many :user_rooms
+  has_many :chats
+  
   has_many :active_relationships, class_name: "Relationship",  #デフォルトではactive_relationshipモデルを探しに行くのでRelationshipモデルを探しに行ってよと設定する必要がある
                                  foreign_key: "follower_id",  #デフォルトでは{モデル}名_idを探しに行くので設定してあげる
                                    dependent: :destroy
-  has_many :followers, through: :active_relationships,
-                         source: :followed
+                                   
+  has_many :followers,               through: :active_relationships,
+                                      source: :followed
   #Userモデル(class)のインスタンスは、followingっていうメソッドを呼び出すことができる。呼び出す時に、through以下とsource以下を活用する。
 
   has_many :passive_relationships, class_name: "Relationship",
                                   foreign_key: "followed_id",
                                     dependent: :destroy
-  has_many :followeds, through: :passive_relationships,
-                        source: :follower
-
+                                    
+  has_many :followeds,                through: :passive_relationships,
+                                       source: :follower
+                        
   has_one_attached :profile_image
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
